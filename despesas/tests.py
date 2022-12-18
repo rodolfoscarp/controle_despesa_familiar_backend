b8fe30320b28f1762d0317b70a5b5d70e97e8c0f
Rodolfo Scarp <rodolfoscarp@gmail.com>
2022-12-18 20:37:47 -0300

    Adicionado testes de receitas e despesas

--- despesas/tests.py
+++ despesas/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from faker import Faker
+from django.urls import reverse
+from despesas.models import Despesa
+from datetime import date
+from decimal import Decimal
 
-# Create your tests here.
+url = reverse('despesa-list')
+
+
+class DespesaViewSetTest(APITestCase):
+
+    def cadastrar_nova_despesa(self):
+
+        despesa = Despesa(
+            descricao="Despesa Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+        despesa.save()
+
+    def test_deve_cadastrar_nova_despesa(self):
+        despesa = dict(
+            descricao="Despesa Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+
+        res = self.client.post(url, despesa, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(despesa['descricao'], Despesa.objects.get().descricao)
+        self.assertAlmostEqual(
+            Decimal(despesa['valor']), Despesa.objects.get().valor)
+        self.assertEqual(despesa['data'], Despesa.objects.get().data)
+
+    def test_nao_deve_cadastrar_duas_despesas_com_mesma_descricao_dentro_do_mesmo_mes(self):
+        despesa_1 = dict(
+            descricao="Despesa Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+
+        despesa_2 = despesa_1.copy()
+        despesa_2['data'] = date(2022, 1, 2)
+
+        self.client.post(url, despesa_1, format='json')
+
+        res = self.client.post(url, despesa_2, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_deve_listar_despesas_cadastradas(self):
+        self.cadastrar_nova_despesa()
+
+        res = self.client.get(url, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.json()), 1)
+
+    def test_deve_atualizar_uma_despesa(self):
+        self.cadastrar_nova_despesa()
+        despesa = Despesa.objects.get()
+
+        data = dict(
+            descricao="Despesa Teste Atualizada",
+            valor=Decimal('150'),
+            data=date(2022, 1, 10)
+        )
+
+        pk = despesa.pk
+
+        res = self.client.put(f'{url}{pk}/', data=data, format='json')
+
+        despesa_atualizada = Despesa.objects.get()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(despesa_atualizada.descricao, data['descricao'])
+        self.assertAlmostEqual(despesa_atualizada.valor, data['valor'])
+        self.assertEqual(despesa_atualizada.data, data['data'])
+
+    def test_deve_deletar_um_despesa(self):
+        self.cadastrar_nova_despesa()
+        despesa = Despesa.objects.get()
+
+        pk = despesa.pk
+
+        res = self.client.delete(f'{url}{pk}/', format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
--- receitas/tests.py
+++ receitas/tests.py
@@ -1,3 +1,91 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from receitas.models import Receita
+from datetime import date
+from decimal import Decimal
 
-# Create your tests here.
+url = reverse('receita-list')
+
+
+class ReceitaViewSetTest(APITestCase):
+
+    def cadastrar_nova_receita(self):
+
+        receita = Receita(
+            descricao="Receita Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+
+        receita.save()
+
+    def test_deve_cadastrar_nova_receita(self):
+        receita = dict(
+            descricao="Receita Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+
+        res = self.client.post(url, receita, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(receita['descricao'], Receita.objects.get().descricao)
+        self.assertAlmostEqual(
+            Decimal(receita['valor']), Receita.objects.get().valor)
+        self.assertEqual(receita['data'], Receita.objects.get().data)
+
+    def test_nao_deve_cadastrar_duas_receitas_com_mesma_descricao_dentro_do_mesmo_mes(self):
+        receita_1 = dict(
+            descricao="Receita Teste",
+            valor=Decimal('100'),
+            data=date(2022, 1, 1)
+        )
+
+        receita_2 = receita_1.copy()
+        receita_2['data'] = date(2022, 1, 2)
+
+        self.client.post(url, receita_1, format='json')
+
+        res = self.client.post(url, receita_2, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_deve_listar_receitas_cadastradas(self):
+        self.cadastrar_nova_receita()
+
+        res = self.client.get(url, format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.json()), 1)
+
+    def test_deve_atualizar_uma_receita(self):
+        self.cadastrar_nova_receita()
+        receita = Receita.objects.get()
+
+        data = dict(
+            descricao="Receita Teste Atualizada",
+            valor=Decimal('150'),
+            data=date(2022, 1, 10)
+        )
+
+        pk = receita.pk
+
+        res = self.client.put(f'{url}{pk}/', data=data, format='json')
+
+        receita_atualizada = Receita.objects.get()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(receita_atualizada.descricao, data['descricao'])
+        self.assertAlmostEqual(receita_atualizada.valor, data['valor'])
+        self.assertEqual(receita_atualizada.data, data['data'])
+
+    def test_deve_deletar_um_receita(self):
+        self.cadastrar_nova_receita()
+        receita = Receita.objects.get()
+
+        pk = receita.pk
+
+        res = self.client.delete(f'{url}{pk}/', format='json')
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
